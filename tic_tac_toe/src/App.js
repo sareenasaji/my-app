311ce8c44fef7c4b7e650371bafd8c0fa17758bb
sareenasaji <sareenasaji16@gmail.com>
2024-04-02 19:45:54 +0530

    tic-tac-toe

---
+++ tic_tac_toe/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Box from './Box.js'; 
+
+function App() {
+  return (
+    <div className="App">
+     <Box />
+    </div>
+  );
+}
+
+export default App;
